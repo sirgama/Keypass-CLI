95de6641c32933c5228046ed0d2025fa22fd7092
sirgama <sirengogamaliel@gmail.com>
2022-04-24 18:59:41 +0300

    create a class for instances of passwords

---
+++ locker.py
@@ -0,0 +1,15 @@
+
+class Credentials:      #class that creates instances of passwords
+    
+    
+    credential_list = [] #list of stored credentials will be here
+    
+    def __init__(self, platform, username, password):
+        
+        self.platform = platform
+        self.username = username
+        self.password = password
+        
+    
+        
+    
