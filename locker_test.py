3c61bf02d1afb02e2cff63f035ba4e9528e36db8
sirgama <sirengogamaliel@gmail.com>
2022-04-24 19:12:12 +0300

    creating new credential test passed

---
+++ locker_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from locker import Credentials
+
+class TestCredentials(unittest.TestCase):
+    def setUp(self):
+        self.new_credentials = Credentials("Instagram","sirgama","42625435")
+        
+        
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
