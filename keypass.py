5dfc183361cab6313ffdd6b463ad6372a70d60df
sirgama <sirengogamaliel@gmail.com>
2022-04-25 01:40:33 +0300

    Add required functions on runner file

--- keypass.py
+++ keypass.py
@@ -2,3 +2,25 @@
 
 from locker import Credentials, User
 
+def create_user(username, password):
+    new_user = User(username, password)
+    return new_user
+
+def save_user(locker):
+    locker.save_user()
+
+def create_credentials(platform, username, password):
+    new_credentials = Credentials(platform, username, password)
+    return new_credentials
+
+def save_credentials(locker):
+    locker.save_credentials()
+    
+def find_credential(social):
+    return Credentials.find_by_platform(social)
+
+def delete_credential(locker):
+    locker.delete_credentials()
+    
+def display_credentials():
+    return Credentials.display_credentials()
