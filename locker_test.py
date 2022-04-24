9d7a9298670ad55ecfd2a16ef66eca0188b45589
sirgama <sirengogamaliel@gmail.com>
2022-04-24 19:15:00 +0300

    create a teardown function

--- locker_test.py
+++ locker_test.py
@@ -3,8 +3,13 @@ import unittest
 from locker import Credentials
 
 class TestCredentials(unittest.TestCase):
+    
     def setUp(self):
-        self.new_credentials = Credentials("Instagram","sirgama","42625435")
+        self.new_credentials = Credentials("Instagram","sirgama","42625435") #creates new credentials sample for testing purposes
+        
+    
+    def tearDown(self):
+        Credentials.credential_list = []
         
         
 
