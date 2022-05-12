43a8cf24894e81adeffc0b05f3bf9bfc45e80dbb
sirgama <sirengogamaliel@gmail.com>
2022-05-12 06:54:58 +0300

    add generate password method

--- locker.py
+++ locker.py
@@ -106,4 +106,7 @@ class Credentials:
         """
         return cls.credential_list
     
-    
+    def generatePassword(stringLength=10):
+        """Generate a random password string of letters and digits and special characters"""
+        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#74%23^*"
+        return ''.join(random.choice(password) for i in range(stringLength))
